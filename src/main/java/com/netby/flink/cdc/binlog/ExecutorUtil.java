60c717fa533e6a8af5e10da9d7de698e3e2d6832
byg <yuangang.byg@alibaba-inc.com>
2023-06-04 16:23:40 +0800

    线程池优化

--- src/main/java/com/netby/flink/cdc/binlog/ExecutorUtil.java
+++ src/main/java/com/netby/flink/cdc/binlog/ExecutorUtil.java
@@ -1,18 +1,29 @@
 package com.netby.flink.cdc.binlog;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
 
 /**
  * @author baiyg
  * @date 2023/1/12 10:59
  */
+@Slf4j
 public class ExecutorUtil {
 
-    private static ExecutorService nonCoreExecutorService = new ThreadPoolExecutor(1, 2, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>(512));
+    private static ExecutorService nonCoreExecutorService =
+            new ThreadPoolExecutor(1, 2, 60L, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>(512), new RejectedExecutionHandler() {
+                @Override
+                public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                    log.error("非核心线程池任务丢失");
+                }
+            });
 
+    /**
+     * 非核心线程池
+     *
+     * @return
+     */
     public static ExecutorService nonCore() {
         return nonCoreExecutorService;
     }
